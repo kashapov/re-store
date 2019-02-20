92c97567c16de3defea947bbc25f495e5264a0c1
Andrey Kashapov <kashapovandrey@gmail.com>
2019-02-20 16:55:17 +0300

    clean code

--- src/actions/index.js
+++ src/actions/index.js
@@ -39,14 +39,6 @@ export const allBooksRemovedFromCart = bookId => {
   };
 };
 
-const fetchBooksOld = (bookstoreService, dispatch) => () => {
-  dispatch(booksRequested());
-  bookstoreService
-    .getBooks()
-    .then(data => dispatch(booksLoaded(data)))
-    .catch(err => dispatch(booksError(err)));
-};
-
 const fetchBooks = bookstoreService => () => dispatch => {
   dispatch(booksRequested());
   bookstoreService
